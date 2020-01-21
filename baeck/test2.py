dfe831fe7db66be612773c4379140386724cfa35
good boy-kr <llaayy54@gmail.com>
2020-01-21 19:29:00 +0900

    0121

--- baeck/test2.py
+++ baeck/test2.py
@@ -1,57 +1,53 @@
-world_size = list(map(int, input().split()))
-stack = []
+world_size = []
 world = []
 visited = []
 count = []
 island = []
-
+num=0
 x = [1, 1, 1, 0, 0, -1, -1, -1]
 y = [1, -1, 0, -1, 1, 1, -1, 0]
 
-if sum(world_size[:]) == 0:
-    exit()
-else:
-    for i in range(world_size[1]):
-        tempmap = list(map(int, input().split()))
-        world.append(tempmap)
 
-for i in range(len(world)):
-    for j in range(len(world[0])):
-        if world[i][j] == 1:
-            island.append([i, j])
-
-
-def move(start):                #start는 [x, y] 로 리스트면서 좌표다
-    now = island.pop(0)
-    add(now)
+def move():
+    global num
+    if len(island) == 0:
+        count.append(num)
+        num = 0
+        world.clear()
+        draw()
+    else:
+        now = island.pop(0)
+        add(now)
+        num = num + 1
+    move()
 
 
 def add(where):                 #where는 현재 좌표 입력 [x,y]
-    num=0
     for i in range(8):
         a =where[0]+x[i]
         b =where[1]+y[i]
         if([a, b] in island and [a,b] not in visited):
-            island.remove([a,b])
+            island.remove([a, b])
             visited.append([a, b])
-            print('visted입니다.', visited)
-            print('island입니다.', island)
     while len(visited)!=0:
         add(visited.pop(0))
-    num=num+1
-    count.append(num)
-    while len(island)!=0:
-        move(island.pop(0))
-
-"""            if k==l:
-                num+=1
-                count.append(num)
-                visited.clear()
-                move(island.pop())"""
-"""add(visited.pop(0))"""
 
 
+def draw():
+    world_size=list(map(int, input().split()))
+    if sum(world_size[:]) == 0:
+        for i in count:
+            print(i)
+        exit()
+    else:
+        for i in range(world_size[1]):
+            tempmap = list(map(int, input().split()))
+            world.append(tempmap)
+        for i in range(len(world)):
+            for j in range(len(world[0])):
+                if world[i][j] == 1:
+                    island.append([i, j])
+    move()
 
-print('아일랜드', island)
-move(1)
-print(count)
+
+draw()
